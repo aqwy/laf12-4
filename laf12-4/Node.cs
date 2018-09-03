b6d3040d36573e22e5137abe406e0bbfba238cb1
Andrey Denisenko <francescogacczi@gmail.com>
2018-09-03 22:03:10 +0300

    change1

---
+++ laf12-4/Node.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laf12_4
+{
+    class Node
+    {
+        public int iData;
+        public Node leftChild;
+        public Node rightChild;
+        public Node (int key)
+        {
+            iData = key;
+        }
+        public int getKey()
+        {
+            return iData;
+        }
+        public void displayNode()
+        {
+            Console.Write(iData);
+        }
+    }
+}
